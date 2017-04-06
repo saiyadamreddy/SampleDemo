7ba91e0c16394577cf551b74f430083723f5e9c0
saiyadamreddy <saiyadamreddy@gmail.com>
2017-04-06 19:16:05 +0530

    Initial commit

---
+++ app/src/main/java/com/sample/demo/view/BookListView.java
@@ -0,0 +1,18 @@
+package com.sample.demo.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sample.demo.R;
+
+/**
+ * Created by Sai on 06-04-2017.
+ */
+
+public class BookListView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.booklist);
+    }
+}
---
+++ app/src/main/java/com/sample/demo/view/GooglePlusLogin.java
@@ -0,0 +1,143 @@
+package com.sample.demo.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.Plus;
+import com.sample.demo.R;
+
+public class GooglePlusLogin extends Activity implements OnClickListener, ConnectionCallbacks, OnConnectionFailedListener {
+
+    private static final int RC_SIGN_IN = 0;
+    // Google client to communicate with Google
+    private GoogleApiClient mGoogleApiClient;
+
+    private boolean mIntentInProgress;
+    private boolean signedInUser;
+    private ConnectionResult mConnectionResult;
+    private SignInButton signinButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+        signinButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signinButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(Plus.API, Plus.PlusOptions.builder().build()).addScope(Plus.SCOPE_PLUS_LOGIN).build();
+        mGoogleApiClient.connect();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    private void resolveSignInError() {
+        if (mConnectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+            } catch (SendIntentException e) {
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!result.hasResolution()) {
+            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, 0).show();
+            return;
+        }
+        if (!mIntentInProgress) {
+            mConnectionResult = result;
+            if (signedInUser) {
+                resolveSignInError();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        switch (requestCode) {
+            case RC_SIGN_IN:
+                if (responseCode == RESULT_OK) {
+                    signedInUser = false;
+                }
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+                if (!mGoogleApiClient.isConnecting()) {
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        signedInUser = false;
+        Toast.makeText(this, "Connected", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(GooglePlusLogin.this, BookListView.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        mGoogleApiClient.connect();
+        Intent intent = new Intent(GooglePlusLogin.this, BookListView.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                googlePlusLogin();
+                break;
+        }
+    }
+
+    public void signIn(View v) {
+        googlePlusLogin();
+    }
+
+    public void logout(View v) {
+        googlePlusLogout();
+    }
+
+    private void googlePlusLogin() {
+        if (!mGoogleApiClient.isConnecting()) {
+            signedInUser = true;
+            resolveSignInError();
+        }
+    }
+
+    private void googlePlusLogout() {
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+            mGoogleApiClient.connect();
+        }
+    }
+}
